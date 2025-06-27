356feb78ae746e2293e0ce0a1dc679c8c18049c6
Arefin_Mizan <137878463+ArefinAlter@users.noreply.github.com>
2025-06-27 15:48:32 +0600

    Updated next.config.js

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig = {
+  swcMinify: false,
+  images: {
+    domains: ['images.pexels.com', 'api.dicebear.com'],
+  },
+  experimental: {
+    optimizeCss: true,
+  },
+}
+
+module.exports = withPWA(nextConfig);
